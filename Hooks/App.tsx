d04bfbd86fc4b91a42ef97631e01a2470dc7ab06
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-03-06 21:40:01 -0300

    useState,useEffect e useReducer

--- Hooks/App.tsx
+++ Hooks/App.tsx
@@ -1,19 +1,55 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState,useEffect,useReducer} from 'react';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+
+
+const reducer = (
+  state:{counter:number},
+  action:{type:string; }) =>{
+  switch(action.type){
+    case 'increment':
+      return{
+        counter: state.counter + 1
+      }
+      case 'decrement':
+        return{
+          counter: state.counter -1
+        }
+      default:
+        return state
+  }
+}
 
 export default function App() {
- const[count,setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer,{counter:0})
+
+  const[count,setCount] = useState(0)
+
+
+
+//  useEffect(()=>{
+//   if(count <= 0){
+//     Alert.alert("Carrinho","Seu carrinho está vazio")
+//     console.log("Carrinho  Vazio")
+//   }else{
+//     console.log( count + " items no carrinho.")
+//   } 
+//  },[count])
 
  const incrementCount = () =>{
-  setCount((prevState)=>prevState + 1)
- }
+  // setCount((prevState)=>prevState + 1)
+  dispatch({type:'increment'})
+  }
 
  const decrementCount = () =>{
-  setCount((prevState)=>prevState - 1)
+  // if(count > 0){
+  // setCount((prevState)=>prevState - 1 )
+  // }
+  dispatch({type:'decrement'})
+
  }
   return (
     <View style={styles.container}>
-      <Text style={{fontSize:40}}>{count}</Text>
+      <Text style={{fontSize:40}}>{state.counter}</Text>
 
       <View style={styles.inline}>
         <Button title='Adicionar' color="white" onPress={incrementCount}></Button>
