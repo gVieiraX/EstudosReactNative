7525a968606e22cec8ccc0ca74e60c957dc835e1
gVieiraX <gabriel_silvarosa12@hotmail.com>
2025-03-08 20:03:03 -0300

    React Navigation

---
+++ ReactNavigation/src/screen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View,Text } from 'react-native';
+
+
+export default function HomeScreen() {
+  return (
+    <View style={{flex:1, backgroundColor:'red'}}>
+         <Text>Home Works</Text>
+    </View>
+  );
+}                   
